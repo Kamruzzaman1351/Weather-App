b4171c5d96aefa089b3a5d09a8ccfa44012d0e82
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-05-11 13:45:33 +0600

    Hide the api key

--- weather.js
+++ weather.js
@@ -1,13 +1,12 @@
-class Weather {
+class Weather extends WeatherM {
     // constructor
     constructor(city) {
-        this.api_key = "8abbfed7abcd6260276c773284fd874c";
-        this.city = city;
+        super(city)
     }
 
     // Weather Update
     async getWeather() {
-        const wResquest = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.api_key}`);
+        const wResquest = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.returnAppKey()}`);
         const wData = await wResquest.json();
         return wData;
     }
